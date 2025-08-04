6e47d913dd4870fe8a9a92ee15a3f335684af8c4
Cody Khor <codykhor@gmail.com>
2025-08-04 19:31:24 +1000

    Define movie schema and insert a few movies

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    await mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
     .then(() => {
         console.log("CONNECTION OPEN")
     }).catch((err) => {
@@ -11,3 +11,24 @@ async function main() {
         console.log(err)
     })
 }
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+// const lilo = new Movie({title: 'Lilo & Stitch', year: 2025, score: 9.2, rating: 'R'})
+
+// Movie.insertMany([
+//     {title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
+//     {title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+//     {title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
+//     {title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
+//     {title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
+// ])
+// .then(data => {
+//     console.log('WORKS', data)
+// })
